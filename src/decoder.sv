494d96c9011b019150c5e09b930fa5e25e8ec0d9
auritron <myself.anath@gmail.com>
2026-07-12 17:26:15 +0530

    opcodes added for rv32i decoder

---
+++ src/decoder.sv
@@ -0,0 +1,8 @@
+module name #(
+    parameter DATA_WIDTH = rv32i::DATA_WIDTH,
+    parameter ADDR_WIDTH = rv32i::ADDR_WIDTH
+) (
+    input 
+);
+    
+endmodule
--- src/rv32i.sv
+++ src/rv32i.sv
@@ -3,4 +3,16 @@ package rv32i;
     localparam DATA_WIDTH = 32;
     localparam ADDR_WIDTH = 5;
 
+    typedef enum logic [6:0] {  //7 bits for opcode
+        OP_R =      7'b0110011,
+        OP_I =      7'b0010011,
+        OP_I_L =    7'b0000011,
+        OP_S =      7'b0100011,
+        OP_B =      7'b1100011,
+        OP_J =      7'b1101111,
+        OP_LUI =    7'b0110111,
+        OP_AUIPC =  7'b0010111,
+        OP_I_E =    7'b1110011
+    } OpCode;
+
 endpackage
