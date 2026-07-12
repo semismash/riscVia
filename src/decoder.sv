e6353b83f6938ab7e893acee7b62bc160a6f65df
auritron <myself.anath@gmail.com>
2026-07-12 21:02:00 +0530

    decoder fields and immediate generator added

--- src/decoder.sv
+++ src/decoder.sv
@@ -1,8 +1,44 @@
-module name #(
+typedef enum logic [2:0] { 
+    I, S, B, U, J
+} ImmPackFmt;
+
+typedef logic [rv32i::DATA_WIDTH-1:0] Instruction;
+typedef logic [rv32i::DATA_WIDTH-1:0] Word;
+
+import rv32i::ALUOpType;
+
+module decoder #(
     parameter DATA_WIDTH = rv32i::DATA_WIDTH,
     parameter ADDR_WIDTH = rv32i::ADDR_WIDTH
 ) (
-    input 
+    input Instruction instr,   // instruction
+    input logic alu_zero,       // feedback from ALU to resolve branch conditions
+
+    output logic reg_write,     // if it writes back to a register
+    output logic mem_write,     // if writes to data memory (for OP_I, OP_I_L, OP_R)
+
+    output ALUOpType alu_op,        // alu specific operation (to be moved into main logic)
+    output logic alu_mem_or_imm,           //picks between alu second input being reg or imm
+
+    output logic mem_to_reg,               // chooses between routing alu output and data mem output to reg file
+    output ImmPackFmt imm_out,             // method to pack the immediate, directive for immediate generator
+
+    output logic pc_target      // PC output control, if PC + 4 or PC + BranchTarget
 );
+
+    import rv32i::ALUOpType;
+
+    always_comb begin
+        case ()
+    end
     
+endmodule
+
+module immediate_generator (
+    input Instruction instr,
+    input ALUOpType alu_op,
+
+    output Word imm_out
+);
+
 endmodule
--- src/rv32i.sv
+++ src/rv32i.sv
@@ -4,15 +4,21 @@ package rv32i;
     localparam ADDR_WIDTH = 5;
 
     typedef enum logic [6:0] {  //7 bits for opcode
-        OP_R =      7'b0110011,
-        OP_I =      7'b0010011,
-        OP_I_L =    7'b0000011,
-        OP_S =      7'b0100011,
-        OP_B =      7'b1100011,
-        OP_J =      7'b1101111,
-        OP_LUI =    7'b0110111,
-        OP_AUIPC =  7'b0010111,
-        OP_I_E =    7'b1110011
+        OP_R      = 7'b0110011,
+        OP_I      = 7'b0010011,
+        OP_I_L    = 7'b0000011,
+        OP_S      = 7'b0100011,
+        OP_B      = 7'b1100011,
+        OP_J      = 7'b1101111,
+        OP_LUI    = 7'b0110111,
+        OP_AUIPC  = 7'b0010111,
+        OP_I_E    = 7'b1110011
     } OpCode;
+
+    typedef enum logic [1:0] {
+        ALU_OP_ADD,
+        ALU_OP_SUB,
+        ALU_OP_LOOK
+    } ALUOpType;
 
 endpackage
