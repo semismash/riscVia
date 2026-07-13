f7ab6c4f8058444b30df41b3a9ad123ca94d90cb
auritron <myself.anath@gmail.com>
2026-07-14 00:50:48 +0530

    initialized core module and moved immediate gen to its own separate module

--- src/decoder.sv
+++ src/decoder.sv
@@ -20,7 +20,9 @@ module decoder #(
     output logic reg_write,     // if it writes back to a register
     output logic mem_write,     // if writes to data memory (for OP_S)
     output logic mem_to_reg,    // chooses between routing alu output (0) and data mem output from load (1) to reg file
-    output Word imm_out,        // output by immediate generator (to be moved into different file later, likely)
+
+    // IMM GEN
+    output ImmPackFmt imm_type, // output imm type for imm gen
 
     // PC
     output logic pcinc_in1_pcor,    // PC inc input 1, default PC (0) or rs1 (1)
@@ -32,11 +34,11 @@ module decoder #(
     OpCode opcode;
     ImmPackFmt imm_type;
 
-    immediate_generator imm_gen (
-        .instr   (instr),
-        .imm_type(imm_type),
-        .imm_out (imm_out)
-    );
+    // immediate_generator imm_gen (
+    //     .instr   (instr),
+    //     .imm_type(imm_type),
+    //     .imm_out (imm_out)
+    // );
 
     function AluOp calc_alu_op();
         logic [2:0] funct3 = instr[14:12];
@@ -154,24 +156,4 @@ module decoder #(
         endcase
     end
     
-endmodule
-
-module immediate_generator (
-    input Instruction instr,
-    input ImmPackFmt imm_type,
-
-    output Word imm_out
-);
-
-    always_comb begin
-        case (imm_type)
-            I: imm_out = {{21{instr[31]}}, instr[30:20]};
-            S: imm_out = {{21{instr[31]}}, {instr[30:25], instr[11:7]}};
-            B: imm_out = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-            U: imm_out = {instr[31:12], {12{1'b0}}};
-            J: imm_out = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:25], instr[24:21], 1'b0};
-            default: imm_out = {32{1'b0}};
-        endcase 
-    end
-
 endmodule
--- src/rv32i_core.sv
+++ src/rv32i_core.sv
@@ -1,5 +1,18 @@
+import rv32i::*;
+
 module rv32i_core (
-    
+    input Instruction instr_in, // next instruction (MEM -> FU)
+    input Word data_in,         // input data from (MEM -> LSU)
+    input logic if_fault,       // instruction fetch fault (MEM -> FU)
+    input logic data_fault,     // data fetch fault (MEM)
+
+    output logic clk,           // CPU clock
+
+    output Word if_addr,        // instruction fetch address (FU)
+    output Word data_addr,      // data address (LSU)
+    output Word data_out,       // data out (LSU)    
+    output logic write_en,      // write enable (LSU)
+    output ReqBytes req_bytes,  // requested bytes amount (LSU)
 );
 
 endmodule
