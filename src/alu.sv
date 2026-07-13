5e90c3baaf7955a51f22f714126aef9cb814a6ae
auritron <myself.anath@gmail.com>
2026-07-14 00:11:21 +0530

    integrated rs1/pc switch in alu module itself

--- src/alu.sv
+++ src/alu.sv
@@ -6,25 +6,32 @@ module alu #(
 )(
     input AluOP alu_op,
 
-    input logic [DATA_WIDTH-1:0] r_data1,
-    input logic [DATA_WIDTH-1:0] r_data2,
+    input Word r_data1,
+    input Word r_data2,
 
-    output logic [DATA_WIDTH-1:0] r_out,
+    input Word pc,
+    input logic use_pc,  // switch between in r_data1 (0) or pc (1) for data1
+
+    output Word r_out,
     output logic out_zero
 );
 
+    Word data1;
+
     always_comb begin
+        if (use_pc == 1'b0) data1 = r_data1;
+        else data1 = pc;
         case (alu_op)
-            ADD: r_out = r_data1 + r_data2;
-            SUB: r_out = r_data1 - r_data2;
-            XOR: r_out = r_data1 ^ r_data2;
-            OR:  r_out = r_data1 | r_data2;
-            AND: r_out = r_data1 & r_data2;
-            SLL: r_out = r_data1 << r_data2[ADDR_WIDTH-1:0];
-            SRL: r_out = r_data1 >> r_data2[ADDR_WIDTH-1:0];
-            SRA: r_out = $signed(r_data1) >>> r_data2[ADDR_WIDTH-1:0];
-            STL: r_out = ($signed(r_data1) < $signed(r_data2)) ? 1 : 0;
-            STLU: r_out = (r_data1 < r_data2) ? 1 : 0;
+            ADD: r_out = data1 + r_data2;
+            SUB: r_out = data1 - r_data2;
+            XOR: r_out = data1 ^ r_data2;
+            OR:  r_out = data1 | r_data2;
+            AND: r_out = data1 & r_data2;
+            SLL: r_out = data1 << r_data2[ADDR_WIDTH-1:0];
+            SRL: r_out = data1 >> r_data2[ADDR_WIDTH-1:0];
+            SRA: r_out = $signed(data1) >>> r_data2[ADDR_WIDTH-1:0];
+            STL: r_out = ($signed(data1) < $signed(r_data2)) ? 1 : 0;
+            STLU: r_out = (data1 < r_data2) ? 1 : 0;
             default: r_out = 0;
         endcase
     end
