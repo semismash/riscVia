7019e4e6fde5965a1f31368087f03a3ec86950ff
auritron <myself.anath@gmail.com>
2026-08-29 19:14:27 +0530

    finished stall unit

--- src/design/core/components/dep_analyzer.sv
+++ src/design/core/components/dep_analyzer.sv
@@ -21,6 +21,14 @@ module dep_analyzer (   // dependency analyzer
     // MEM/WB
     output logic dep_mem_wb_rd_id_ex_rs1,
     output logic dep_mem_wb_rd_id_ex_rs1,
+
+    // extra bits to check if its not zero regiter
+    output logic if_id_rs1_not_x0,
+    output logic if_id_rs2_not_x0,
+    output logic id_ex_rd_not_x0,
+    // check if rs1 or rs2 usage is even valid
+    output logic rs1_valid,
+    output logic rs2_valid,
 );
 
     // for stalls (catches load-use early)
@@ -33,5 +41,24 @@ module dep_analyzer (   // dependency analyzer
     
     assign dep_mem_wb_rd_id_ex_rs1 = (mem_wb_rd == id_ex_rs1);
     assign dep_mem_wb_rd_id_ex_rs2 = (mem_wb_rd == id_ex_rs2);
+
+    // non-zero check
+    assign if_id_rs1_not_x0 = (if_id_rs1 != '0);
+    assign if_id_rs2_not_x0 = (if_id_rs2 != '0);
+    assign id_ex_rd_not_x0  = (if_id_rd  != '0);
+
+    // rs1 & rs2 validity check
+    assign rs1_valid = 
+        (if_id_opcode == OP_R)      ||
+        (if_id_opcode == OP_I)      ||
+        (if_id_opcode == OP_I_L)    ||
+        (if_id_opcode == OP_S)      ||
+        (if_id_opcode == OP_B)      ||
+        (if_id_opcode == OP_I_J)    ||
+        (if_id_opcode == OP_I_E);
+    assign rs2_valid = 
+        (if_id_opcode == OP_R)      ||
+        (if_id_opcode == OP_S)      ||
+        (if_id_opcode == OP_B);
 
 endmodule
