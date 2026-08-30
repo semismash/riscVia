519686e74941f9f481cc24f6bde195d44d49be2a
auritron <myself.anath@gmail.com>
2026-08-31 02:27:20 +0530

    performed tests, 42/45 testcases passed, lot of them with 1 CPI now, but the stores failed, need to debug and see what happened

--- program.s
+++ program.s
@@ -1,5 +1,5 @@
 #
-# TEST CODE FOR BLT
+# TEST CODE FOR XORI
 #
         # -----------------------------------------
         # Program section (known as text)
@@ -15,25 +15,20 @@ _start: .global _start
 # Label for entry point of test code
 main:
         ### TEST CODE STARTS HERE ###
+       
+        # 100 ^ 150 = 242 (nonzero answer)
+        li      x1, 100          # set x1 to 100 (0x00000064)
+        xori    x2, x1, 150      # xori x1(100) to 150, x2=242 (0x000000F2) 
         
-        # compare positive numbers
-        li      x1, 50          # set x1 to 50 (0x00000032)
-        li      x2, 100         # set x2 to 100 (0x00000064)
+        # 100 ^ 100 = 0 (zero answer)
+        xori    x3, x1, 100      # xor x1(100) to 100, x3=0 (0x00000000)
         
-        beq     x1, x0, fail0   # make sure x1 has value 
-        blt     x1, x2, branch1 # if x1 < x2, branch to branch1
-        j       fail1           # jump to fail
-        
-        branch1: 
-        blt     x2, x1, fail1   # if x2 < x1, branch to fail
-        
-        # compare signed numbers
-        li      x3, -50         # set x3 to -50 (0xFFFFFFCE)
-        blt     x1, x3, fail2   # if x3 < x1, branch to fail
-
-        # compare equal numbers
-        blt     x3, x3, fail3
-        
+        #self-check  
+        li      x4, 242         # set x4 to 242 (expected value for x3)
+        beqz    x4, fail0       # make sure x4 has value
+        bne     x2, x4, fail1   #
+        bnez    x3, fail2       # branch to fail if not equal to expected value
+         
         ###    END OF TEST CODE   ###
 
         # Exit test using RISC-V International's riscv-tests pass/fail criteria
@@ -57,12 +52,6 @@ main:
         li      a7, 93          # reached end of code
         ebreak
         
-        fail3:
-        li      a0, 6           # fail code
-        li      a7, 93          # reached end of code
-        ebreak
-
-
         # -----------------------------------------
         # Data section. Note starts at 0x1000, as 
         # set by DATAADDR variable in rv_asm.bat.
