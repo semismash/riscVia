df9be3d960ffcf80677cc93f03f0b07b362bcbfb
auritron <myself.anath@gmail.com>
2026-09-04 01:16:12 +0530

    mispredictor logic complete

--- src/design/core/branch_predictor/branch_eval.sv
+++ src/design/core/branch_predictor/branch_eval.sv
@@ -4,10 +4,16 @@ module branch_eval( // purely combinational
     // INPUTS:
     input BranchConfidenceState conf_state_in,  // from PHT
     input logic btb_hit,                        // from BTB
-    input PCAddrNoUnused btb_target_addr,       // from BTB
+    input BTBEntry btb_entry,                   // from BTB
     // OUTPUTS:
     output logic take_branch,               // (0 = don't take, 1 = take)
     output PCAddrNoUnused branch_addr       // update PC with target address
 );
+
+    always_comb begin
+        take_branch = 1'b0;
+        branch_addr = btb_entry.target_addr;
+        if (btb_hit && (BranchConfidenceState == STRONG_T || BranchConfidenceState == WEAK_T)) take_branch = 1'b1;
+    end
 
 endmodule
